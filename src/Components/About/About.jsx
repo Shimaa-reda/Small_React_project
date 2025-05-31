ed555203aff80ed91d73eddf333ac7a95ba96ac2
Shimaa Reda <81780715+Shimaa-reda@users.noreply.github.com>
2025-05-31 19:31:48 +0300

    about component

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -3,7 +3,32 @@ import React from 'react'
 export default function About() {
   return <>
   
-  <h1>About page</h1>
+  <div className="home d-flex justify-content-center align-items-center flex-column vh-100">
+        
+         <div className="content pt-4 text-center">
+           <h2 className="text-uppercase text-white fw-bolder fs-1 mb-3">
+             about component
+           </h2>
+           <div className="line_star d-flex justify-content-center align-items-center mb-3">
+             <div className="line me-2"></div>
+             <i class="fa-solid fa-star text-white"></i>
+             <div className="line ms-2"></div>
+           </div>
+           </div>
+           <div className="container text-white">
+                 <div className="row px-5">
+            <div className="col-sm-12 col-md-6 ps-md-5">
+                <p>Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
+            </div>
+
+             <div className="col-sm-12 col-md-6 pe-md-5">
+                <p>Freelancer is a free bootstrap theme created by Route. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
+            </div>
+           </div>
+           </div>
+          
+       
+       </div>
   </>
     
 }
