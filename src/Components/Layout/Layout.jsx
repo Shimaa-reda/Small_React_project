e350eec0832362cea198823e8102719e97795531
Shimaa Reda <81780715+Shimaa-reda@users.noreply.github.com>
2025-07-22 13:23:26 +0300

    updates

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import Navbar from '../Navbar/navbar'
+// import Navbar from '../Navbar/navbar'
 import Footer from '../Footer/Footer'
 import { Outlet } from 'react-router-dom'
+import Navbar from "../Navbar/Navbar";
+
 
 export default function Layout() {
   return (
